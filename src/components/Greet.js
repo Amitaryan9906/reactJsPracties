ca18bca96d90ec1a0c12b9f560275c2b88f72a9f
Amit-Das <damit5221@gmail.com>
2024-01-10 11:25:01 +0530

    Updated

---
+++ src/components/Greet.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Greet=()=> <h1>Welcome to the new react project</h1>
---
+++ src/components/Hello.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HelloMessage=()=>{
+return React.createElement('div',{id:'hello',className:'HelloMessage'},React.createElement('h1',null,'Hello Amit!'))
+};
